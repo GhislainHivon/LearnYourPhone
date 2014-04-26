9d0041a1fe3e92287d59633b096935515dc1d36f
Ghislain Hivon <ghislain.hivon@gmail.com>
2014-04-26 14:36:12 -0400

    Create a skeleton for the Learn your phone app

---
+++ learnyourphone/main.py
@@ -0,0 +1,15 @@
+# encoding: utf-8
+
+"""Main for the Learn Your Phone app"""
+
+from __future__ import unicode_literals
+
+from kivy.app import App
+
+
+class LearnYourPhoneApp(App):
+    pass
+
+
+if __name__ == '__main__':
+    LearnYourPhoneApp().run()
