a9bf641b6d68698bb542258617d7ab059f37cfa3
Ghislain Hivon <ghislain.hivon@gmail.com>
2014-04-29 21:20:10 -0400

    Add settings for the phone number

--- learnyourphone/main.py
+++ learnyourphone/main.py
@@ -6,9 +6,27 @@ from __future__ import unicode_literals
 
 from kivy.app import App
 
+SETTINGS_SECTION = "Learn your phone"
+SETTINGS_KEY_PHONE = "number"
 
 class LearnYourPhoneApp(App):
-    pass
+
+    def build_config(self, config):
+        config.setdefaults(SETTINGS_SECTION,
+                           {SETTINGS_KEY_PHONE: ""})
+        return super(LearnYourPhoneApp, self).build_config(config)
+    
+    def build_settings(self, settings):
+        jsondata = """[
+                       {{"type": "string",
+                         "title": "Phone number",
+                         "desc": "The complete phone number you want to learn",
+                         "section": "{section}",
+                         "key": "{key}"
+                       }}
+                      ]""".format(section=SETTINGS_SECTION, key=SETTINGS_KEY_PHONE)
+        settings.add_json_panel('Learn your phone', self.config, data=jsondata)
+        return super(LearnYourPhoneApp, self).build_settings(settings)
 
 
 if __name__ == '__main__':
