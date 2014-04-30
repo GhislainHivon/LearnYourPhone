2107b3cb9317a0969eed33e1b3d2dbf82395b39b
Ghislain Hivon <ghislain.hivon@gmail.com>
2014-04-29 23:16:27 -0400

    First version of validating input

--- learnyourphone/main.py
+++ learnyourphone/main.py
@@ -2,14 +2,39 @@
 
 """Main for the Learn Your Phone app"""
 
+# To check : https://mail.python.org/pipermail/python-list/2008-February/494675.html
+# And kivy accept hsv color 
+# http://kivy.org/docs/api-kivy.graphics.html#kivy.graphics.Color
+
 from __future__ import unicode_literals
 
 from kivy.app import App
+from kivy.uix.textinput import TextInput
 
 SETTINGS_SECTION = "Learn your phone"
 SETTINGS_KEY_PHONE = "number"
 
+class ValidatingTextinput(TextInput):
+    """A self validating textinput: refusing all text that is not expecting"""
+
+    def __init__(self, **kwargs):
+        self.expecting_text = kwargs.pop("expecting")
+        self.last_text = kwargs.get("text", "")
+        super(ValidatingTextinput, self).__init__(**kwargs)
+        self.bind(text=self.on_text)
+
+    def on_text(self, instance, value):
+        if value:
+            if value == self.expecting_text:
+                print "good value"
+            else:
+                print "not good value"
+                self.text = self.last_text
+        self.last_text = self.text
+        
+
 class LearnYourPhoneApp(App):
+    """The app to learn your phone number"""
 
     @property
     def spacer(self):
@@ -27,7 +52,12 @@ class LearnYourPhoneApp(App):
         self.answer_layout.clear_widgets()
         self.hint_layout.clear_widgets()
         if phone_number:
-            self.spacer.text = phone_number
+            self.spacer.text = ""
+            for digit in phone_number:
+                digit_input = ValidatingTextinput(expecting=digit,
+                                                  multiline=False)
+                self.answer_layout.add_widget(digit_input)
+
         else:
             self.spacer.text = "Set the phone number in the settings."
 
